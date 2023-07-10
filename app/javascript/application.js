b20996f644a2b7342adc337fadfd90cbc2a6ad5a
shubham <shubham.bhoyar077@gmail.com>
2023-07-10 17:33:07 +0530

    Add App and index component

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,13 @@
 // Entry point for the build script in your package.json
-import '@hotwired/turbo-rails';
-import './controllers';
+import "@hotwired/turbo-rails";
+import "./controllers";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+// function App() {
+//   return <h1>Hello World!</h1>;
+// }
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
